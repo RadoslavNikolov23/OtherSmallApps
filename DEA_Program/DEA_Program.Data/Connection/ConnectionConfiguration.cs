4d5632cd9aaed7da38ebcc0130f069f7e48f92fc
Radoslav Nikolov <156640386+RadoslavNikolov23@users.noreply.github.com>
2025-07-23 17:21:18 +0300

    Update ConnectionConfiguration.cs

--- DEA_Program/DEA_Program.Data/Connection/ConnectionConfiguration.cs
+++ DEA_Program/DEA_Program.Data/Connection/ConnectionConfiguration.cs
@@ -3,7 +3,7 @@
     public static class ConnectionConfiguration
     {
         public static string ConnectionPath = 
-            @"Server=127.0.0.1;Port=5432;Database=DEA_ProgramDB;User Id=postgres;Password=123123;";
+            @"Server=127.0.0.1;Port=5432;Database=DEA_ProgramDB;User Id=...;Password=.....;";
 
     }
 }
